1c3b0a44626cf57fb1e8eeb452e6c61ba98a33ee
Mark Dyson <mark.dyson1@outlook.com>
2020-04-28 12:28:00 +0000

    debug turned back on due to app crashing

--- app.py
+++ app.py
@@ -90,4 +90,4 @@ def get_details(podcast_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=False)
+            debug=True)
